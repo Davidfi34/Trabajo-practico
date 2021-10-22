25a61ff91b74d324a295e99c090fc89f69551da6
David <figuererodavid@gmail.com>
2021-10-22 20:38:45 -0300

    Segundo commit- tests de empleado eventual(comiciones,ingresos totales y montos negativos)

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -9,6 +9,33 @@ class EmpleadoEventualTest extends EmpleadoTest
       $empEv = new \App\EmpleadoEventual($nombre,$apellido,$dni,$salario,$monto);
       return $empEv; 
     }
+
+
+//Probar que el método calcularComision() funciona como se espera.  
+public function testCalcularComision()
+{
+  $empEv = $this->crear('nombre','apellido',1234,3,[25,30,50]);
+  $this-> assertEquals(1.75, $empEv->calcularComision());
+}
+
+
+//Probar que el método calcularIngresoTotal() funciona como se espera.
+public function testCalcularIngresoTotal()
+{
+  $empEv = $this->crear('nombre','apellido',1234,3,[25,30,50]);
+  $this-> assertEquals(4.75, $empEv->calcularIngresoTotal());
+}
+
+
+/*Probar que si intento construir un empleado proporcionando algún monto 
+de venta negativo o cero, lanza una excepción.*/
+public function testMontoDeVentaNegativoOenCero()
+{
+  $this->expectException(\Exception::class);
+  $empEv = $this->crear('nombre','apellido',1234,100,[-25,-30,-50,0]);
+}
+
+
 }
 
 
